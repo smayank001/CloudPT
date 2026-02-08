7d338866063cc46212678663b71e338cbf65f454
Mayank saxena <mayanksaxena1262@gmail.com>
2026-02-09 01:12:53 +0530

    h

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,7 +21,7 @@ import heroVideo from "@/assets/video.mp4";
 import PartnerMarquee from "@/components/PartnerMarquee";
 import { useState, useEffect } from "react";
 import homeee from "@/assets/homee.jpg";
-import reviewImage from "@/assets/review.jpg";
+import reviewImage from "@/assets/g.jpg";
 
 const Home = () => {
   const features = [
